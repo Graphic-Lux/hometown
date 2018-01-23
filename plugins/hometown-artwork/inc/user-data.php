0678b9fe14843f775fa035024019796155e6f1d1
Sumner <serhard2556@gmail.com>
2018-01-23 01:06:52 -0700

    Setup image url and color variables to be sent to database via ajax

--- plugins/hometown-artwork/inc/user-data.php
+++ plugins/hometown-artwork/inc/user-data.php
@@ -2,29 +2,30 @@
 
 add_action( 'wp_ajax_hometown_save_imprint_artwork', 'hometown_save_imprint_artwork' );
 /**
- * Save the imprint artwork to the database
+ * Save the imprint artwork url and color to the database
  *
  * return @void
  */
 function hometown_save_imprint_artwork() {
-  echo 'asdf';
+	$uniqueIdentifier = $_POST['variation_id'];
 
-//  $uniqueIdentifier = $_POST['variation_id'];
-//
-//  $front = (isset($_POST['front'])) ? 'Front='.$_POST['front'] : '';
-//  $back = (isset($_POST['back'])) ? 'Back='.$_POST['back'] : '';
-//  $sleeve = (isset($_POST['sleeve'])) ? 'Sleeve='.$_POST['sleeve'] : '';
-//
-//  $meta_key = 'imprint_locations-' . $uniqueIdentifier;
-//  $imprintCSV = $front.','.$back.','.$sleeve;
-//
-//  $prev_value = get_user_meta(get_current_user_id(), $meta_key, true);
-//
-//  wp_send_json(array(
-//      'action' => 'save_imprint_data',
-//      'result' => (update_user_meta( get_current_user_id(), $meta_key, $imprintCSV, $prev_value )),
-//      'newID'  => update_user_meta( get_current_user_id(), $meta_key, $imprintCSV, $prev_value )
-//  ));
-//  wp_die();
+	$frontURL    = ( isset( $_POST['frontURL'] ) ) ? 'FrontURL=' . $_POST['frontURL'] : '';
+	$frontColor  = ( isset( $_POST['frontColor'] ) ) ? 'FrontColor=' . $_POST['frontColor'] : '';
+	$backURL     = ( isset( $_POST['backURL'] ) ) ? 'BackURL=' . $_POST['backURL'] : '';
+	$backColor   = ( isset( $_POST['backColor'] ) ) ? 'BackColor=' . $_POST['backColor'] : '';
+	$sleeveURL   = ( isset( $_POST['sleeveURL'] ) ) ? 'SleeveURL=' . $_POST['sleeveURL'] : '';
+	$sleeveColor = ( isset( $_POST['sleeveColor'] ) ) ? 'SleeveColor=' . $_POST['sleeveColor'] : '';
+
+	$meta_key   = 'imprint_artwork-' . $uniqueIdentifier;
+	$imprintCSV = $frontURL . ',' . $frontColor . ',' . $backURL . ',' . $backColor . ',' . $sleeveURL . ',' . $sleeveColor;
+
+	$prev_value = get_user_meta( get_current_user_id(), $meta_key, true );
+
+	wp_send_json( array(
+		'action' => 'save_imprint_artwork',
+		'result' => ( update_user_meta( get_current_user_id(), $meta_key, $imprintCSV, $prev_value ) ),
+		'newID'  => update_user_meta( get_current_user_id(), $meta_key, $imprintCSV, $prev_value )
+	) );
+	wp_die();
 
 }
