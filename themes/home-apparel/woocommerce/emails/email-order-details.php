83983436e189d2f3a1d8ad1fb0c076f3226d421c
Matthew Lux <graphiclux@gmail.com>
2018-03-01 16:23:59 -0500

    Fixed Order link issue

--- themes/home-apparel/woocommerce/emails/email-order-details.php
+++ themes/home-apparel/woocommerce/emails/email-order-details.php
@@ -30,6 +30,6 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 	<h2><a class="link" href="<?php echo esc_url( admin_url( 'post.php?post=' . $order->get_id() . '&action=edit' ) ); ?>"><?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?></a> (<?php printf( '<time datetime="%s">%s</time>', $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ); ?>)</h2>
 <?php endif; ?>
 
-<a href="<?php echo get_site_url() ?> /my-account/view-order/<?php echo $order->get_order_number() ?>">Access your order details.</a>
+<a href="<?php echo get_site_url() ?>/my-account/view-order/<?php echo $order->get_order_number() ?>">Access your order details.</a>
 
 <?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
