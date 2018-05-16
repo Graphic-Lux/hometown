00ddca99226f6c8fb5421cab9329fba50a72bb94
Grayson Erhard <grayson@graphiclux.com>
2018-05-15 18:52:28 -0600

    added abspath security and if the user isn't logged in, they'll be redirected back to the homepage

--- plugins/hometown-api/views/custom/create.php
+++ plugins/hometown-api/views/custom/create.php
@@ -1,3 +1,15 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit; // Exit if accessed directly
+}
+
+if (get_current_user_id() < 1) {
+  wp_redirect( get_site_url() );
+  exit;
+}
+?>
+
 <?php get_header(); ?>
 
 <div id="create" class="avia-section main_color avia-section-default avia-no-shadow avia-bg-style-scroll  avia-builder-el-0  el_before_av_section  avia-builder-el-first   container_wrap fullsize">
